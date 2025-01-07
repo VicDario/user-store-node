42e23f6083d86c7c9395d61ded7fb9fceaefd170
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-07 15:12:47 -0300

    feat: init auth controller and routes

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,17 @@
+import type { Request, Response } from "express";
+
+export class AuthController {
+    constructor() {}
+
+    registerUser = (request: Request, res: Response) => {
+
+    }
+
+    loginUser = (request: Request, res: Response) => {
+
+    }
+
+    validateEmail = (request: Request, res: Response) => {
+
+    }
+}
