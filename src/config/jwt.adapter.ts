1dd1277d041eeddb37b9c5317294beca46ee8b38
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-08 02:50:03 -0300

    feat: add jwt adapter

---
+++ src/config/jwt.adapter.ts
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
+
+export class JwtAdapter {
+    static generateToken(payload: any, duration: string = '2h') {
+        return new Promise((resolve) => {
+            jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (error, token) => {
+                if (error) return resolve(null);
+                resolve(token)
+            }); 
+        })
+    }
+
+    static validateToken(token: string) {
+        return jwt.verify(token, JWT_SEED);
+    }
+}
