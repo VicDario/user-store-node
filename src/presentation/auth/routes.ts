b7d99fff579b3671f722dcc8bdcd82b07c22e6c1
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-09 02:08:37 -0300

    feat: inject email service to auth service

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import { AuthController } from './controller';
-import { AuthService } from '../services/auth.service';
+import { AuthService, EmailService } from '../services';
+import { envs } from '../../config';
 
 export class AuthRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const authService = new AuthService();
+    const emailService = new EmailService({
+      mailerEmail: envs.MAILER_EMAIL,
+      mailerSecretKey: envs.MAILER_SECRET_KEY,
+      mailerService: envs.MAILER_SERVICE
+    });
+    const authService = new AuthService(emailService);
     const controller = new AuthController(authService);
     
     router.post('/login', controller.loginUser);
