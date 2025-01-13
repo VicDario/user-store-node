7165902021b20c062dba482e37040c760bebb25b
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-13 16:00:10 -0300

    feat: inject service and implement

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,18 +1,22 @@
 import type { NextFunction, Request, Response } from 'express';
 import { CreateCategoryDto, CustomError } from '../../domain';
+import { CategoryService } from '../services/category.service';
 
 export class CategoryController {
-  constructor() {}
+  constructor(private categoryService: CategoryService) {}
 
-  getCategories = (req: Request, res: Response) => {
-    res.json('Get Category')
+  getCategories = (req: Request, res: Response, next: NextFunction) => {
+    this.categoryService.getCategories()
+      .then((categories) => res.status(200).json(categories))
+      .catch((error) => next(error));
   }
 
   createCategory = (req: Request, res: Response, next: NextFunction) => {
     const [error, createCategoryDto] = CreateCategoryDto.create(req.body);
-
     if (error) next(CustomError.badRequest(error));
 
-    res.json('Create Category')
+    this.categoryService.createCategory(createCategoryDto!, req.body.user)
+      .then((category) => res.status(201).json(category))
+      .catch((error) => next(error));
   }
 }
--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { CategoryController } from './controller';
 import { AuthMiddleware } from '../middlewares/auth.middleware';
+import { CategoryService } from '../services/category.service';
 
 export class CategoryRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const controller = new CategoryController();
+    const categoryService = new CategoryService();
+    const controller = new CategoryController(categoryService);
 
     router.get('/', controller.getCategories);
     router.post('/', [AuthMiddleware.validateJWT], controller.createCategory);
