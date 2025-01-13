118d88c6fc2dfce4af07ff620a1422e98f55de49
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-12 23:28:18 -0300

    feat: use generics to type method

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -9,15 +9,15 @@ export class JwtAdapter {
             jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (error, token) => {
                 if (error) return resolve(null);
                 resolve(token)
-            }); 
+            });
         })
     }
 
-    static validateToken(token: string) {
+    static validateToken<T>(token: string): Promise<T | null> {
         return new Promise((resolve) => {
             jwt.verify(token, JWT_SEED, (error, decoded) => {
                 if (error) resolve(null);
-                resolve(decoded);
+                resolve(decoded as T);
             })
         })
     }
