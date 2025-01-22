7309e39ace4db54a448d5c6e3b0234a8e6c29f3f
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-22 14:11:20 -0300

    feat: update barrel file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,7 @@
-export * from './envs';
-export * from './regular-exp';
 export * from './bcrypt.adapter';
+export * from './envs';
 export * from './jwt.adapter';
+export * from './regular-exp';
+export * from './uuid.adapter';
 export * from './validators';
+
