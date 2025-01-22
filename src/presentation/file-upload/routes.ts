92193cb7cc265ce1999f2ec3285c41af650b2946
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-22 15:15:32 -0300

    feat: use middleware

--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { FileUploadController } from './controller';
 import { FileUploadService } from '../services';
+import { FileUploadMiddleware } from '../middlewares/file-upload.middleware';
 
 export class FileUploadRoutes {
   static get routes(): Router {
@@ -8,6 +9,8 @@ export class FileUploadRoutes {
 
     const service = new FileUploadService();
     const controller = new FileUploadController(service);
+
+    router.use(FileUploadMiddleware.containFiles);
 
     router.post('/single/:type', controller.uploadFile);
     router.post('/multiple/:type', controller.uploadFile);
