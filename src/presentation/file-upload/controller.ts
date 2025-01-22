c74ed60ef5d69ba19a6ae35d89ff3bfbc88c909d
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-22 20:29:45 -0300

    MINOR: delete unused import

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -1,6 +1,5 @@
 import type { NextFunction, Request, Response } from 'express';
 import { FileUploadService } from '../services';
-import { CustomError } from '../../domain';
 import { UploadedFile } from 'express-fileupload';
 
 export class FileUploadController {
