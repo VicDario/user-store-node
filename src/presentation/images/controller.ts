00085b2f3899f00e9648fce917e3a723807cc386
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-23 14:59:53 -0300

    fix: correct images path

--- src/presentation/images/controller.ts
+++ src/presentation/images/controller.ts
@@ -8,7 +8,7 @@ export class ImageController {
 
   getImage = (req: Request, res: Response, next: NextFunction) => {
     const { type = '', img = '' } = req.params;
-    const imagePath = path.resolve(__dirname, '../../../uploads', type, img);
+    const imagePath = path.resolve(__dirname, '../../../public/uploads', type, img);
     if (!existsSync(imagePath)) {
       next(CustomError.notFound('Image not found'));
       return;
