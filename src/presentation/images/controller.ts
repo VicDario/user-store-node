9a8ff96810924ce811357823985606c0d690f0f6
VicDario <26585356+VicDario@users.noreply.github.com>
2025-01-23 14:41:32 -0300

    feat: add image route

---
+++ src/presentation/images/controller.ts
@@ -0,0 +1,19 @@
+import type { NextFunction, Request, Response } from 'express';
+import { existsSync } from 'fs';
+import path from 'path';
+import { CustomError } from '../../domain';
+
+export class ImageController {
+  constructor() {}
+
+  getImage = (req: Request, res: Response, next: NextFunction) => {
+    const { type = '', img = '' } = req.params;
+    const imagePath = path.resolve(__dirname, '../../../uploads', type, img);
+    if (!existsSync(imagePath)) {
+      next(CustomError.notFound('Image not found'));
+      return;
+    }
+
+    res.sendFile(imagePath);
+  };
+}
